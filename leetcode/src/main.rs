275c221ac7575ad04397cd8e65935c52ce363dd0
hjtianvip <hjtianvip@gmail.com>
2019-09-21 16:41:12 +0800

    Add leetcode p0009_palindrome_number

---
+++ leetcode/src/main.rs
@@ -0,0 +1,6 @@
+pub mod p0009_palindrome_number;
+
+
+fn main() {
+    println!("Hello, rust and leetcode!");
+}
---
+++ leetcode/src/p0009_palindrome_number.rs
@@ -0,0 +1,40 @@
+pub struct Solution {}
+
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x >= 0 && x <= 9 { return true}
+        if x < 0 || x % 10 == 0 { return false}
+        let mut digits: Vec<i32> = Vec::new();
+        let mut input = x;
+        while input != 0 {
+            digits.push(input % 10);
+            input /= 10;
+        }
+        let len = digits.len();
+        let mut index = 0;
+        while index < len/2 {
+            if digits[index] != digits[len-index-1] {return false}
+            index += 1;
+        }
+        true
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_palindrome_test() {
+        assert_eq!(Solution::is_palindrome(0), true);
+        assert_eq!(Solution::is_palindrome(9), true);
+        assert_eq!(Solution::is_palindrome(-32), false);
+        assert_eq!(Solution::is_palindrome(100), false);
+        assert_eq!(Solution::is_palindrome(121), true);
+        assert_eq!(Solution::is_palindrome(1321), false);
+        assert_eq!(Solution::is_palindrome(2222), true);
+        assert_eq!(Solution::is_palindrome(11222211), true);
+    }
+}
