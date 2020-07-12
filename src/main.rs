5e430532bdb2e50824d813cbfb8329129dcd5e56
hjtianvip <hjtianvip@gmail.com>
2020-07-12 13:17:42 +0800

    Add dp src/p1513_number_of_substrings_with_only_1s.rs

--- src/main.rs
+++ src/main.rs
@@ -87,6 +87,7 @@ pub mod p0350_intersection_of_two_array_ii;
 pub mod p0455_assign_cookies;
 pub mod p0523_continuous_subarray_sum;
 pub mod p1512_number_of_good_pairs;
+pub mod p1513_number_of_substrings_with_only_1s;
 
 // 堆栈
 pub mod p0020_valid_parentheses;
---
+++ src/p1513_number_of_substrings_with_only_1s.rs
@@ -0,0 +1,70 @@
+// 1513. Number of Substrings With Only 1s
+// Medium
+
+// Given a binary string s (a string consisting only of '0' and '1's).
+
+// Return the number of substrings with all characters 1's.
+
+// Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+// Example 1:
+
+// Input: s = "0110111"
+// Output: 9
+// Explanation: There are 9 substring in total with only 1's characters.
+// "1" -> 5 times.
+// "11" -> 3 times.
+// "111" -> 1 time.
+// Example 2:
+
+// Input: s = "101"
+// Output: 2
+// Explanation: Substring "1" is shown 2 times in s.
+// Example 3:
+
+// Input: s = "111111"
+// Output: 21
+// Explanation: Each substring contains only 1's characters.
+// Example 4:
+
+// Input: s = "000"
+// Output: 0
+ 
+
+// Constraints:
+
+// s[i] == '0' or s[i] == '1'
+// 1 <= s.length <= 10^5
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn num_sub(s: String) -> i32 {
+        let mut dp: Vec<i64> = vec![0; s.len()];
+        let chs: Vec<char> = s.chars().collect();
+        for i in 0..s.len() {
+            if i == 0 {
+                dp[i] = if chs[i] == '1' { 1 } else { 0 };
+            } else if chs[i-1] == '1' && chs[i] == '1'{
+                dp[i] = (dp[i] + dp[i-1] + 1) % 1000000007
+            } else if chs[i] == '1' {
+                dp[i] = 1;
+            }
+        }
+        dp.iter().fold(0, |result, x| (result + x) % 1000000007 ) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn num_sub_test() {
+        assert_eq!(Solution::num_sub("000".to_owned()), 0);
+        assert_eq!(Solution::num_sub("0110111".to_owned()), 9);
+        assert_eq!(Solution::num_sub("111111".to_owned()), 21);
+    }
+}
